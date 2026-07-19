5108911bcf5e38a7d945718f688013457ae8ebe5
laksna yoga <laksnayoga@laksnas-MacBook-Pro.local>
2026-07-19 08:46:52 -0700

    fix: allow vercel domain in cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
+        'http://localhost:5173',
+        'https://projek-dpsi-perpustakan.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
